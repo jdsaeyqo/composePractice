f72ee95e85f4f76cf30dcf00b3560129964ca8bf
SANG\jdsae <jdsaeyqi@naver.com>
2022-10-19 19:18:14 +0900

    Movie App

---
+++ MovieApp/app/src/main/java/com/example/movieapp/navigation/MovieScreens.kt
@@ -0,0 +1,18 @@
+package com.example.movieapp.navigation
+
+import java.lang.IllegalArgumentException
+
+//www.google.com/sing_in
+enum class MovieScreens {
+    HomeScreen,
+    DetailScreen;
+
+    companion object {
+        fun fromRoute(route: String?): MovieScreens = when (route?.substringBefore("/")){
+            HomeScreen.name -> HomeScreen
+            DetailScreen.name -> DetailScreen
+            null -> HomeScreen
+            else ->  throw IllegalArgumentException("Route $route is not recognize")
+        }
+    }
+}
---
+++ MovieApp/app/src/main/java/com/example/movieapp/screens/home/HomeScreen.kt
@@ -0,0 +1,8 @@
+package com.example.movieapp.screens.home
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun HomeScreen(){
+
+}
